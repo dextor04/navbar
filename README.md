79442a8e45a923d1b2fae13f53b8f867cbb1c97e
dextor04 <kk448800333@gmail.com>
2023-04-16 14:22:34 +0530

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+reference:https://youtu.be/bfOTRUZEJ2k
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import { FiMenu, FiX } from "react-icons/fi";
 
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+  const handleClick = () => setOpen(!open);
+  const closeMenu = () => setOpen(false);
   return (
     <nav className="navbar">
       <Link to="/" className="nav-logo">
         NavBar
       </Link>
-      <ul className="nav-links">
-        <li>
+      <div className="nav-icons" onClick={handleClick}>
+        {open ? <FiX /> : <FiMenu />}
+        {/* <FiMenu />
+        <FiX /> */}
+      </div>
+      <ul className={open ? "nav-links active" : "nav-links"}>
+        <li className="nav-item" onClick={closeMenu}>
           <Link to="/" className="nav-link">
             Home
           </Link>
         </li>
-        <li>
+        <li className="nav-item" onClick={closeMenu}>
           <Link to="/about" className="nav-link">
             About
           </Link>
         </li>
-        <li>
+        <li className="nav-item" onClick={closeMenu}>
           <Link to="/contact" className="nav-link">
             Contact
           </Link>
         </li>
-        <li>
+        <li className="nav-item" onClick={closeMenu}>
           <Link to="/shop" className="nav-link">
             Shop
           </Link>
